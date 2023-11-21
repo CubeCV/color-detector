37892e774710d07221259acd5f7b75f37dde2bf9
xyntechx <xyntechx@gmail.com>
2023-11-21 21:46:49 +0000

    Move datasets to /datasets

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from ultralytics import YOLO
 
+# Load a model
+model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data='data.yaml', epochs=100)
+
+# Evaluate the model on val set
+metrics = model.val()
+
+# Export the model to .pt file
+success = model.export()
