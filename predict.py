f050eda1faee548005e93807291e881c4eafb5c0
xyntechx <xyntechx@gmail.com>
2023-11-22 16:43:11 +0000

    Add predict.py and cleanup /runs

---
+++ predict.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import argparse
+
+
+def parse_option():
+    parser = argparse.ArgumentParser("Predict with color detector model", add_help=False)
+    parser.add_argument("--model", type=str, required=True, metavar="FILE", help="Path to model")
+    parser.add_argument("--image", type=str, required=True, metavar="FILE", help="Path to image")
+    
+    args = parser.parse_args()
+
+    return args
+
+
+def main(model_path, image_path):
+    model = YOLO(model_path)
+
+    # Predict on image
+    results = model(image_path)
+
+
+if __name__ == "__main__":
+    args = parse_option()
+    main(args.model, args.image)
